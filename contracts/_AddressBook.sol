712efb8d2701761f964f07bf1bd2b0a95a2d9d7f
sanjipun <iamsanjipun@gmail.com>
2022-01-11 21:20:50 +0545

    first commit

---
+++ contracts/_AddressBook.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
+
+contract AddressBook {
+    using SafeMath for uint;
+}
